d6383d00fdfbd519315ddc1f97818ff9e77e49f8
david.carrus <david.carrus@gucci.com>
2022-02-09 23:26:43 +0100

    flipping chars depending on position

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import styled from "styled-components";
 import {Background} from "./components/Background";
 import {Character} from "./components/Character";
 import {getPageData} from "./service/getPageData";
@@ -8,12 +7,13 @@ import {getPageData} from "./service/getPageData";
 const pageData = getPageData();
 
 function App() {
-  return (
-    <div className="App">
-      <Background imageUrl={pageData.background.img} text={""}/>
-      <Character imageUrl={pageData.char01.img} name={pageData.char01.name} text={""} />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Background imageUrl={pageData.background.img} text={""}/>
+                <Character imageUrl={pageData.char01.img} name={pageData.char01.name} text={""} position={"right"}/>
+                <Character imageUrl={pageData.char02.img} name={pageData.char02.name} text={""} position={"left"} />
+        </div>
+    );
 }
 
 export default App;
--- src/components/Character.tsx
+++ src/components/Character.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import styled from "styled-components";
 
-const CharacterWrapper = styled.div`
+const CharacterWrapper = styled.div<Character>`
     position: absolute;
     bottom: 0;
-    right: 0;
+    ${props => props.position === 'left' ? 
+        `transform: scaleX(-1);
+        left:0;`:''};
+    ${props => props.position === 'right' ?
+    `right:0;`:''};
     width: 30vw;
     z-index: 10;
     img {
@@ -18,11 +22,12 @@ interface Character {
     imageUrl: string;
     name: string
     text: string | null;
+    position: string | null;
 }
 
-export const Character = ({imageUrl,name,text}:Character) => {
+export const Character = ({imageUrl,name,text,position}:Character) => {
     return (
-        <CharacterWrapper>
+        <CharacterWrapper imageUrl={imageUrl} name={name} text={text} position={position}>
             <img src={imageUrl} />
         </CharacterWrapper>
     )
